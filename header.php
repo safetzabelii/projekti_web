22394885c015cc96c22ca5a67ed19227c3a7bd6b
safetzabelii <sz51042@ubt-uni.net>
2022-02-27 16:59:54 +0100

    header php

---
+++ header.php
@@ -0,0 +1,31 @@
+
+<?php
+include_once('link.php');
+
+?>
+<html>
+<head>
+	<style>
+		li{
+			text-align:center;
+			color: black;
+    font-size: 20px;
+    height: 20px;
+	list-style: none;
+	text-decoration: none;
+		}
+	</style>
+</head>
+
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Registration Login</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="registration.php">Registration</a></li>
+			<li><a href="login.php">Login</a></li>
+		</ul>
+	</div>
+</nav>
+</html>
