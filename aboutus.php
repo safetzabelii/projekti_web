e0a79ae509647761199977f0c82c28977f77c70f
AltinMurtezaj <am51230@ubt-uni.net>
2022-02-27 17:03:35 +0100

    aboutus

---
+++ aboutus.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="aboutus.css">
+
+</head>
+<br><br>
+<br>
+<br>
+    <nav>
+
+        <a href="#" class="logo">
+            <img src="icons/Movie-roll-cinema-entertainment-icon-EPS-10-by-Hoeda80.jpg" />
+        </a>
+
+        <input type="checkbox" class="menu-btn" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn">
+            <span class="nav-icon"></span>
+        </label>
+
+        <ul class="menu">
+            <li><a href="homepage.php">Home</a></li>
+            <li><a href="movie.php">Movies</a></li>
+            <li><a href="tvshow.php">TV Shows</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="#">Categories</a></li>
+            <li>
+                <div class="search">
+                    <input type="text" placeholder="Find Your Favourtie Movies" />
+
+                    <i class="fas fa-search"></i>
+                </div>
+            </li>
+
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+        </nav>
+        
+<body>
+    
+
+
+
+    
+    <div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+					<h1>About Us</h1>
+				</div>
+				<div class="content">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aut repudiandae, pariatur laborum voluptatum, quasi quis voluptate quaerat accusantium ipsam optio odit. Nesciunt veritatis commodi, modi quod laborum voluptatum perferendis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptatem excepturi similique hic quae, ipsam sed id quisquam molestiae inventore incidunt odio magnam saepe quam aspernatur impedit nam cum est. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel voluptas numquam qui molestias natus expedita, dignissimos adipisci, eveniet provident temporibus quis. Ex odit officiis ea ullam blanditiis tenetur voluptatum architecto? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic itaque, iste rem ducimus dolore officiis facilis. Inventore, similique distinctio! Distinctio minima cupiditate nulla sed iste, fuga labore similique nesciunt rem!</p>
+					
+				</div>
+				<div class="social">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+    
+
+
+
+</body>
+</html>
