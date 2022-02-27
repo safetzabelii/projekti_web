304ab196c76c94a5d03f41514a59d1b15e8a2fee
safetzabelii <sz51042@ubt-uni.net>
2022-02-27 16:52:00 +0100

    Link

---
+++ link.php
@@ -0,0 +1,4 @@
+
+<link rel="stylesheet" type="text/css" href="css/registration.css">
+<link rel="stylesheet" type="text/css" href="css/account.css">
+<script type="text/javascript" src="register.js"></script>
